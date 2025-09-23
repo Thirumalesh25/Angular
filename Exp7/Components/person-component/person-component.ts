4718f239e27c6fec200744c8dce8c8dd32608ca8
Thirumalesh25 <229x1a0540@gprec.ac.in>
2025-09-23 11:41:47 +0530

    Add files via upload

---
+++ Exp7/Components/person-component/person-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-component',
+  imports: [],
+  templateUrl: './person-component.html',
+  styleUrl: './person-component.css'
+})
+export class PersonComponent {
+
+}
