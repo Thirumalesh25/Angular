e6d7c59961c4f2f2b1f07e4a1d6d4f7c1175d7b0
Thirumalesh25 <229x1a0540@gprec.ac.in>
2025-10-28 22:04:57 +0530

    Update app.ts

--- Exp7/app.ts
+++ Exp7/app.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet, RouterLink
-  ],
+  imports: [RouterLink, RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrls: ["./app.css"],
+  styleUrls: ['./app.css']
 })
-export class App {}
+export class App {
+  showNavbar = false;
+
+  toggleNavbar() {
+    this.showNavbar = !this.showNavbar;
+  }
+}
